16f93ff453232ec5aad085fa4daf0feb57b2c290
Yaroslav Bemov <yaroslav.bemov@gmail.com>
2021-08-11 12:33:44 +0300

    add Message component

---
+++ frontend/src/components/Message/Message.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import styles from './message.module.css'
+
+const Message = ({ from = 'Anonymous', text = 'No message' }) => {
+  return (
+    <div className={styles.message}>
+      <h3 className={styles.h3}>{from}</h3>
+      <p>{text}</p>
+    </div>
+  )
+}
+
+export default Message
