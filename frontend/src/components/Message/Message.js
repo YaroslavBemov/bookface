8c41fa7b576ad4fb9ca0196292ba09683f698b32
Yaroslav Bemov <yaroslav.bemov@gmail.com>
2021-08-14 20:56:14 +0300

    improve chat

--- frontend/src/components/Message/Message.js
+++ frontend/src/components/Message/Message.js
@@ -1,11 +1,17 @@
 import React from 'react'
-import styles from './message.module.css'
+import styles from './Message.module.css'
+import classNames from 'classnames'
 
-const Message = ({ from = 'Anonymous', text = 'No message' }) => {
+const Message = ({from = 'Anonymous', text = 'No message'}) => {
+  const align = from === 'Monica' ? styles.mla : null
+  const messageStyles = classNames(styles.message, align)
+
   return (
-    <div className={ styles.message }>
-      <h3 className={ styles.h3 }>{ from }</h3>
-      <p>{ text }</p>
+    <div className={messageStyles}>
+      <div className={styles.content}>
+        <h3 className={styles.h3}>{from}</h3>
+        <p>{text}</p>
+      </div>
     </div>
   )
 }
