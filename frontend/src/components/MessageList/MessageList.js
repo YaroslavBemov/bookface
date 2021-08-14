96ff04ed2129a93267df5369203bb65d27750b36
Yaroslav Bemov <yaroslav.bemov@gmail.com>
2021-08-14 20:56:14 +0300

    improve chat

--- frontend/src/components/MessageList/MessageList.js
+++ frontend/src/components/MessageList/MessageList.js
@@ -1,34 +1,53 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Message from '../Message'
-import styles from './message-list.module.css'
+import styles from './Message-list.module.css'
+import Button from '../Button'
+import InputText from '../InputText'
 
-function MessageList({ list, setList }) {
-  const [input, setInput] = useState({})
-  console.log(input)
+function MessageList({list, setList}) {
+  const [inputText, setInputText] = useState({})
+  const [isDisabled, setIsDisabled] = useState(true)
 
-  const handleInput = (e) => {
-    setInput({ text: e.target.value })
+  const handleChange = (e) => {
+    setInputText({text: e.target.value})
+    setIsDisabled(e.target.value === '')
   }
   const handleKeyPress = (e) => {
-    if (e.key === 'Enter') {
+    if (e.key === 'Enter' && e.target.value !== '') {
       handleClick()
     }
   }
   const handleClick = () => {
-    setList(prev => [...prev, input])
-    setInput({ text: '' })
+    setList(prev => [...prev, inputText])
+    setInputText({text: ''})
+    setIsDisabled(true)
   }
 
   return (
     <>
-      <div className={ styles.message - list }>
-        { list.map(message => (
-          <Message { ...message }/>
-        )) }
+      <div className={styles.messageList}>
+        <div className={styles.messages}>
+          {list.map(message => (
+            <Message {...message}/>
+          ))}
+        </div>
+
+        <div className={styles.input}>
+          <InputText
+            classes={styles.text}
+            value={inputText.text}
+            onChange={handleChange}
+            onKeyPress={handleKeyPress}
+          />
+          <Button
+            classes={styles.button}
+            // icon={'send'}
+            title="Send"
+            onClick={handleClick}
+            disabled={isDisabled}
+          />
+        </div>
       </div>
-      <input type="text" value={ input.text } onChange={ handleInput }
-             onKeyPress={ handleKeyPress }/>
-      <button onClick={ handleClick }>Send</button>
     </>
   )
 }
