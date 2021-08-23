e0235d021a09a62aa22b15dbbe43c6b680e69906
Yaroslav <yaroslav.bemov@gmail.com>
2021-08-23 11:06:27 +0300

    add auth middleware

--- backend/router/index.js
+++ backend/router/index.js
@@ -4,20 +4,24 @@ const { body } = require('express-validator')
 const articleController = require('../controllers/ArticleController')
 const userController = require('../controllers/UserController')
 
+const authMiddleware = require('../middleware/AuthMiddleware')
+
 const router = new Router()
 
 router.post('/signup',
   body('email').isEmail(),
   body('password').isLength({ min: 3, max: 32 }),
   userController.signUp)
+
 router.post('/signin',
   body('email').isEmail(),
   body('password').isLength({ min: 3, max: 32 }),
   userController.signIn)
+
 router.post('/signout', userController.signOut)
 router.get('/activate/:link', userController.activate)
 router.get('/refresh', userController.refresh)
 
-router.get('/articles', articleController.index)
+router.get('/articles', authMiddleware, articleController.index)
 
 module.exports = router
