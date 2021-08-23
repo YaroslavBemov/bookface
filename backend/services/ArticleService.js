f7343f2311c4b9792e1fc3827c8778f2e5f8841b
Yaroslav <yaroslav.bemov@gmail.com>
2021-08-23 11:06:11 +0300

    add article dummy

--- backend/services/ArticleService.js
+++ backend/services/ArticleService.js
@@ -1,4 +1,27 @@
+const ArticleModel = require('../models/ArticleModel')
+
 class ArticleService {
+  async getAllArticles () {
+    // const articles = await ArticleModel.find()
+
+    return [
+      {
+        id: 1,
+        title: 'Lorem 1',
+        description: 'Lorem description 1'
+      },
+      {
+        id: 2,
+        title: 'Lorem 2',
+        description: 'Lorem description 2'
+      },
+      {
+        id: 3,
+        title: 'Lorem 3',
+        description: 'Lorem description 3'
+      }
+    ]
+  }
 
 }
 
