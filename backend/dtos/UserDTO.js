9beb4ff45c5d6c0e737eb5cc538fbaa0d4102f9b
Yaroslav <yaroslav.bemov@gmail.com>
2021-08-27 14:31:05 +0300

    add name field to user

--- backend/dtos/UserDTO.js
+++ backend/dtos/UserDTO.js
@@ -1,10 +1,12 @@
 module.exports = class UserDTO {
   id
+  name
   email
   isActivated
 
   constructor (model) {
     this.id = model._id
+    this.name = model.name
     this.email = model.email
     this.isActivated = model.isActivated
   }
