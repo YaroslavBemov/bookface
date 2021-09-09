fc13b6d2208b0be547536e1700a4261c57c36b54
Yaroslav <yaroslav.bemov@gmail.com>
2021-09-09 10:37:50 +0300

    add new chat

--- frontend/src/pages/Chat.js
+++ frontend/src/pages/Chat.js
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect } from 'react'
 import { Context } from '../index'
-import ChatList from '../components/ChatList/ChatList'
 import {
+  Box,
   Card,
   CircularProgress,
   Container,
@@ -14,23 +14,40 @@ import { makeStyles } from '@material-ui/core/styles'
 import CardHeader from '@material-ui/core/CardHeader'
 import Avatar from '@material-ui/core/Avatar'
 import IconButton from '@material-ui/core/IconButton'
-import ChatIcon from '@material-ui/icons/Chat'
 import { red } from '@material-ui/core/colors'
+import clsx from 'clsx'
 
 const useStyles = makeStyles((theme) => ({
-  root: {
-    maxWidth: '100%',
-    cursor: 'pointer'
-  },
   paper: {
     flexGrow: 1,
     padding: theme.spacing(1),
     textAlign: 'center',
     color: theme.palette.text.secondary
   },
+  chatList: {
+    flexGrow: 1,
+    padding: theme.spacing(1)
+  },
+  chatItem: {
+    width: '100%',
+    flexGrow: 1,
+    cursor: 'pointer'
+  },
+  chatCard: {
+    '&:hover': {
+      backgroundColor: theme.palette.action.hover
+    }
+  },
+  selected: {
+    backgroundColor: theme.palette.action.hover
+  },
+  messageList: {
+    flexGrow: 1,
+    padding: theme.spacing(1)
+  },
   avatar: {
     backgroundColor: red[500]
-  },
+  }
 }))
 
 const Chat = () => {
@@ -47,29 +64,37 @@ const Chat = () => {
     if (chatStore.isErrors) return <div>Unable to display chats.</div>
 
     return chatStore.getChatList.map(chat => (
-      <Card
-        // onClick={}
-        className={classes.root}
+      <Grid item
+            key={chat.id}
+            className={classes.chatItem}
       >
-        <CardHeader
-          avatar={
-            <Avatar aria-label="recipe" className={classes.avatar}>
-              U
-            </Avatar>
-          }
-          action={
-            <IconButton
-              aria-label="settings"
-              // onClick={}
-            >
-              <ChatIcon/>
-            </IconButton>
-          }
-          title={'123'}
-          // subheader="September 14, 2016"
-        />
-        </Card>
+        <Card
 
+          onClick={() => chatStore.setCurrentChatId(chat.id)}
+          className={clsx(
+            classes.chatCard,
+            chatStore.currentChatId === chat.id ? classes.selected : null
+          )}
+        >
+          <CardHeader
+            avatar={
+              <Avatar aria-label="recipe" className={classes.avatar}>
+                U
+              </Avatar>
+            }
+            // action={
+            // <IconButton
+            //   aria-label="settings"
+            //   // onClick={}
+            // >
+            //   {/*<ChatIcon/>*/}
+            // </IconButton>
+            // }
+            title={chat.party[0].name}
+            // subheader="September 14, 2016"
+          />
+        </Card>
+      </Grid>
       // {/*<ChatList*/}
       // {/*  key={chat.id}*/}
       // {/*  id={chat.id}*/}
@@ -85,28 +110,36 @@ const Chat = () => {
 
     return chatStore.getCurrentChatMessages.map(message => (
       <Paper key={message._id} className={classes.paper}>
-      <MessageList
-        key={message._id}
-        message={message}
-      />
+        <MessageList
+          key={message._id}
+          message={message}
+        />
       </Paper>
     ))
   }
 
   return (
     <Container maxWidth="md">
-      <Grid container spacing={1}>
-        <Grid container item xs={4} spacing={3}>
-          {/*<Paper className={classes.paper}>*/}
+      <Box
+        sx={{
+          backgroundColor: 'background.default',
+          minHeight: '100%'
+          // py: 3
+        }}
+      >
+        <Grid container spacing={1}>
+          <Grid container item xs={4}
+            // spacing={3}
+                className={classes.chatList}>
             {renderChatList()}
-          {/*</Paper>*/}
-        </Grid>
-        <Grid container item xs={8} spacing={3}>
-          {/*<Paper className={classes.paper}>*/}
+          </Grid>
+          <Grid container item xs={8}
+                spacing={3}
+                className={classes.messageList}>
             {renderMessageList()}
-          {/*</Paper>*/}
+          </Grid>
         </Grid>
-      </Grid>
+      </Box>
     </Container>
   )
 }
